7b7086c6be15c12e35d6c75736dff3552b247826
Tony Lam <tony.lam@iki.fi>
2025-04-15 22:29:05 +0300

    Feat: tell if it is going to rain on eve and may day

--- src/app.py
+++ src/app.py
@@ -1,8 +1,22 @@
 from flask import Flask
 
+from src.lib.weather import will_it_rain
+from datetime import datetime
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+
+    current_year = datetime.now().year
+    may_eve_rains = will_it_rain(f"{current_year}-04-30")
+    may_day_rains = will_it_rain(f"{current_year}-05-01")
+
+    return "<p>Sataako vappuna?</p> <p>Vappuaattona: {may_eve_rains}</p> <p>Vappupäivänä: {may_day_rains}</p>".format(
+        may_eve_rains=may_eve_rains, may_day_rains=may_day_rains
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/lib/weather.py
+++ src/lib/weather.py
@@ -1,12 +1,12 @@
 import requests
 from datetime import datetime
 
+
 def get_weather():
-    current_year = datetime.now().year
     weather_url = "https://api.open-meteo.com/v1/forecast?latitude=60.1582&longitude=24.9597&hourly=temperature_2m,precipitation_probability,precipitation&timezone=Europe%2FMoscow&forecast_days=16"
     weather_response = requests.get(weather_url)
-
     weather_data = weather_response.json()
+
     weather_time_list = weather_data["hourly"]["time"]
     temperature_list = weather_data["hourly"]["temperature_2m"]
     precipitation_probability_list = weather_data["hourly"]["precipitation_probability"]
@@ -19,9 +19,19 @@ def get_weather():
             "precipitation": precipitation,
         }
         for time, temperature, precipitation_probability, precipitation in zip(
-            weather_time_list, temperature_list, precipitation_probability_list, precipitation_list
+            weather_time_list,
+            temperature_list,
+            precipitation_probability_list,
+            precipitation_list,
         )
     }
+
+    return weather_dict
+
+
+def get_filtered_weather_for_mayday():
+    current_year = datetime.now().year
+    weather_dict = get_weather()
 
     filtered_weather = {
         time: data
@@ -30,3 +40,21 @@ def get_weather():
     }
 
     return filtered_weather
+
+
+def will_it_rain(date_str):
+    """
+    args  - date_str: str - date in YYYY-MM-DD format
+    return - boolean that says if it will rain or not on that day
+    """
+    weather_dict = get_weather()
+
+    will_it_rain = False
+
+    for time, data in weather_dict.items():
+        if time[:10] == date_str:
+            if data["precipitation"] > 0:
+                will_it_rain = True
+                break
+
+    return will_it_rain
