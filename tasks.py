48b0489f5142176cd855879747f0e8599c394c59
Tony Lam <tony.lam@iki.fi>
2025-04-17 22:43:17 +0300

    Feat: add gunicorn as a dependency and update task to start server with gunicorn

--- tasks.py
+++ tasks.py
@@ -1,8 +1,17 @@
 from invoke import task
 
+
 @task
 def start(c):
     """
     Start the Flask development server.
     """
     c.run("poetry run flask --app src.app run --debug")
+
+
+@task
+def server(c):
+    """
+    Start the Flask development server.
+    """
+    c.run("gunicorn -w 4 'src.app:app'")
