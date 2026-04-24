a7d2cefe9ac65cf9aa89ab532b2f150011a15c14
Tony Lam <tony.lam@iki.fi>
2026-04-24 10:26:57 +0300

    update: add better translation

--- src/app.py
+++ src/app.py
@@ -13,10 +13,15 @@ def index():
     may_eve_rains = will_it_rain(f"{current_year}-04-30")
     may_day_rains = will_it_rain(f"{current_year}-05-01")
 
-    if may_eve_rains == None:
+    if may_eve_rains is None:
         may_eve_rains = "No data available"
-    if may_day_rains == None:
+    else:
+        may_eve_rains = "sataa" if may_eve_rains else "ei sada"
+
+    if may_day_rains is None:
         may_day_rains = "No data available"
+    else:
+        may_day_rains = "sataa" if may_day_rains else "ei sada"
 
     may_eve_rain_times = when_will_it_rain(f"{current_year}-04-30")
     may_day_rain_times = when_will_it_rain(f"{current_year}-05-01")
